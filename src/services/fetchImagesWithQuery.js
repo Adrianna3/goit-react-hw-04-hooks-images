4535ae79f58354a6e73d6c6ce10fbbcafa6aa5e9
Adrianna Staniek <adrstani@wp.pl>
2022-12-17 23:55:39 +0100

    copy hom 3

---
+++ src/services/fetchImagesWithQuery.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://pixabay.com/api/";
+
+export const fetchImagesWithQuery = async (searchQuery, page) => {
+  try {
+    const params = new URLSearchParams({
+      key: '28305156-2c31ea34b0b957161935cfaa0',
+      q: searchQuery,
+      lang: 'pl',
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 12,
+      page,
+    });
+  
+    const response = await axios.get(`?${params}`);
+    return response.data;
+  } catch (error) {
+    console.log(`${error.name}: ${error.message}`);
+  }
+};
